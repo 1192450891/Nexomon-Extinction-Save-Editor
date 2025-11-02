a7e583a5dd91bef7095abf341bd5d932141a6708
1192450891 <>
2025-11-02 16:50:36 +0800

    修复矿石数据结构的错误
    fix issuse1

--- Models/Mineral.cs
+++ Models/Mineral.cs
@@ -5,13 +5,13 @@ namespace Save_Editor.Resources.Models
     public class Mineral
     {
         public short id    { get; set; }
-        
-        public bool isDigged    { get; set; }
-        
-        public short type    { get; set; }
+        public short times    { get; set; }
         
         public short unknownData1    { get; set; }
         public bool unknownData2    { get; set; }
+        
+        public short unknownData3    { get; set; }
+        public bool unknownData4    { get; set; }
     }
     
     public static partial class Extensions {
@@ -20,27 +20,29 @@ namespace Save_Editor.Resources.Models
             var mineral = new Mineral
             {
                 id = reader.ReadInt16(),
-                isDigged = reader.ReadBoolean()
+                times  = reader.ReadInt16()
             };
-            if (mineral.isDigged)
+            for (int i = 0; i < mineral.times; i++)
             {
-                mineral.type = reader.ReadInt16();
                 mineral.unknownData1 = reader.ReadInt16();
+                mineral.unknownData2 = reader.ReadBoolean();
+                mineral.unknownData3 = reader.ReadInt16();
+                mineral.unknownData4 = reader.ReadBoolean();
             }
-            mineral.unknownData2 = reader.ReadBoolean();
-
+            
             return mineral;
         }
 
         public static void Write(this BinaryWriter writer, Mineral mineral) {
             writer.Write(mineral.id);
-            writer.Write(mineral.isDigged);
-            if (mineral.isDigged)
+            writer.Write(mineral.times);
+            for (int i = 0; i < mineral.times; i++)
             {
-                writer.Write(mineral.type);
                 writer.Write(mineral.unknownData1);
+                writer.Write(mineral.unknownData2);
+                writer.Write(mineral.unknownData3);
+                writer.Write(mineral.unknownData4);
             }
-            writer.Write(mineral.unknownData2);
         }
     }
 }
